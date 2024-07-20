e234a3f8e9a0949f4ff71b82c9a545812e547d32
Bruno Do Rosario Petrucci - student <petrucci@nova.its.iastate.edu>
2024-07-20 13:08:25 -0500

    Made a mistake on the hisse scripts, trying to solve them.

---
+++ revbayes/scripts/run_rb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --nodes=1 # 1 node per job
+#SBATCH --time=15-00:00:00 # no idea how long this will take, let's be safe 
+
+#SBATCH --output=output/jobs/job_%j.out
+#SBATCH --error=output/jobs/job_%j.err
+
+#SBATCH --job-name="canidae_fbdhisse"
+
+#SBATCH --mail-user=petrucci@iastate.edu   # my e-mail
+#SBATCH --mail-type=BEGIN # get notifications for all job cases
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# load modules
+module purge
+module load revbayes/dev-tp-sa-fix.tensorphylo
+
+# go to the correct directory
+cd /work/LAS/phylo-lab/petrucci/canids_chap2/revbayes
+
+# source it, the parameter combination, and the actual script
+rb scripts/fbdhisse.Rev 
