ad426c5aff844fa2b05448374f533e4c7276b46e
brpetrucci <petrucci@iastate.edu>
2025-02-28 19:40:22 -0600

    Changed some naming stuff on array script.

--- revbayes/scripts/run_rb.sh
+++ revbayes/scripts/run_rb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --nodes=1 # 1 node per job
-#SBATCH --time=01:00:00 # just an hour--calling the array itself doesn't take long
+#SBATCH --time=30-00:00:00 # not sure how long it'll take
 #SBATCH --array=1-18 # 18 parameter combinations
 
 #SBATCH --output=output/bisse/jobs/array/job_%A_%a.out
 #SBATCH --error=output/bisse/jobs/array/job_%A_%a.err
 
-#SBATCH --job-name="array_fbdsse_canids"
+#SBATCH --job-name="fbdsse_canids"
 
 #SBATCH --mail-user=petrucci@iastate.edu   # my e-mail
 #SBATCH --mail-type=BEGIN # get notifications for all job cases
